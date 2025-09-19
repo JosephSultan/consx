5c3d3a808466f4408c2763298dee300e2bd00b95
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2025-09-20 00:15:41 +0200

    d

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -51,7 +51,7 @@ const Hero = () => {
                                 <span className="px-2 py-[0.5px] h-[18px] tracking-wide flex items-center justify-center rounded-full bg-gradient-to-r from-sky-400 to-blue-600 text-[9px] font-medium mr-2 text-white">
                                     NEW
                                 </span>
-                                Explore the 2024 recap
+                                Explore our latest app
                             </span>
                         </button>
                     </Container>
@@ -59,21 +59,21 @@ const Hero = () => {
                         <h1 className="text-4xl md:text-4xl lg:text-7xl font-bold text-center !leading-tight max-w-4xl mx-auto">
                             Transform your {" "}
                             <span className="">
-                                marketing {" "}
+                                Customer Experience {" "}
                             </span>
-                            with AI Precision
+                            with Expert Consultinghelp businesses set up Intercom, implement Fin AI, optimize workflows, cut costs, and boost customer satisfaction
                         </h1>
                     </Container>
                     <Container delay={0.2}>
                         <p className="max-w-xl mx-auto mt-2 text-base lg:text-lg text-center text-muted-foreground">
-                            AI-powered automation and insights to maximize your campaigns and grow your brand.
+                          We help businesses optimize Intercom, set up their workflows, cut costs, and boost customer satisfaction.
                         </p>
                     </Container>
                     <Container delay={0.25} className="z-20">
                         <div className="flex items-center justify-center mt-6 gap-x-4">
                             <Link href="#" className="flex items-center gap-2 group">
                                 <Button size="lg">
-                                    Start Free Trial
+                                    Get in touch
                                     <ArrowRightIcon className="size-4 group-hover:translate-x-1 transition-all duration-300" />
                                 </Button>
                             </Link>
